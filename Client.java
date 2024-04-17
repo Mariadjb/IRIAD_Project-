a65e78a25abe77153ace8891bd08a58ba430948f
Fadouaslm <fadouaslama@gmail.com>
2024-04-17 10:47:22 +0100

    version semi final

--- Client.java
+++ Client.java
@@ -1,32 +1,10 @@
-import java.io.*;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
-public class Client {
-    public static void main(String[] args) {
-        try {
-            // Retrieve the remote object (stub) of the Ministere service
-            Registry registry = LocateRegistry.getRegistry("localhost", 50010);
-            MinistereService etablissementService = (MinistereService) registry.lookup("EtablissementService");
-
-            // Read the file and convert to byte array
-            File file = new File("Data_sets (1).xlsx");
-            byte[] fileData = new byte[(int) file.length()];
-            FileInputStream fis = new FileInputStream(file);
-            fis.read(fileData);
-            fis.close();
- // Send the file to the server and receive the modified file with predictions
- byte[] receivedFileData = etablissementService.predict(fileData, "Data_sets (1).xlsx");
-
- // Save the modified file
- FileOutputStream fos = new FileOutputStream("Modified_Data_sets (1).xlsx");
- fos.write(receivedFileData);
- fos.close();
-
- System.out.println("Received file with predictions saved as Modified_Data_sets (1).xlsx.");
-
-} catch (Exception e) {
- e.printStackTrace();
- }
-}
+public interface Client extends Remote {
+  
+   void receiveProcessedFile(String processedData) throws RemoteException ;
+   public void sendTrainingFile(String filePath, String fileName, String clientId) throws RemoteException;
+   public void sendPredictionFile(String filePath, String fileName, String clientId) throws RemoteException;
+   void receiveFile(byte[] fileData, String fileName, String clientId, String sendingType) throws RemoteException;
 }
--- MinistereService.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface MinistereService extends Remote {
-    void sendFile(byte[] fileData, String fileName) throws RemoteException;
-    String requestPrediction(String studentID, double[] marks) throws RemoteException;
-    byte[] predict(byte[] fileData, String fileName) throws RemoteException;
-}
